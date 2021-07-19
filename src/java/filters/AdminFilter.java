9e1ed9bd62e6c71e142a0101382a46cebd9d7360
840288 <840288@SAIT225697IT.ACDM.DS.SAIT.CA>
2021-07-19 09:19:31 -0600

    Second Commit Worked on Admin Filter doFilter was not working with previous method so changed the doFilter method so that it is now working. Lab Complete.

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -5,6 +5,7 @@
  */
 package filters;
 
+import dataaccess.UserDB;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
@@ -18,7 +19,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import models.User;
+
 
 /**
  *
@@ -28,7 +29,6 @@ public class AdminFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        
         HttpServletRequest httpRequest = (HttpServletRequest)request;
         HttpSession session = httpRequest.getSession();
         String email = (String)session.getAttribute("email");
@@ -39,16 +39,15 @@ public class AdminFilter implements Filter {
             return; // stop code call so that next line of code does not execute.
         }
         
-        User user = (User) session.getAttribute("user");
-        if (user.getRole().getRoleId() != 1) {
+        UserDB userDB = new UserDB();
+        int role = userDB.get(email).getRole().getRoleId();
+        if (role != 1) {
             HttpServletResponse httpResponse = (HttpServletResponse)response;
             httpResponse.sendRedirect("notes");
             return;
         }
-
         chain.doFilter(request,response);
     }
-    
     
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
